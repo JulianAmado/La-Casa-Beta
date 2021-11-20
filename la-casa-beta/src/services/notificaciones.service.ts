43f31d1936c37fc015149ecf8ed04c31a20711b4
NeylaVillamizar <neylavillamizar@yahoo.es>
2021-11-20 14:53:30 -0500

    Archivo .env

--- la-casa-beta/src/services/notificaciones.service.ts
+++ la-casa-beta/src/services/notificaciones.service.ts
@@ -1,12 +1,12 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+require('dotenv').config();
 @injectable({scope: BindingScope.TRANSIENT})
 export class NotificacionesService {
   constructor(/* Add @inject to inject parameters */) {}
 
   notificacionesPorSMS():void {
-    const accountSid = 'ACbe3bd4f6acd6c025873be9084adcc04e'; // Your Account SID from www.twilio.com/console
-    const authToken = '08d26ed9f34d6f26e4e10f01f09c5710'; // Your Auth Token from www.twilio.com/console
+    const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+    const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
     const twilio = require('twilio');
     const client = new twilio(accountSid, authToken);
@@ -19,10 +19,10 @@ export class NotificacionesService {
       })
       .then((message:any) => console.log(message.sid));
       }
-
+      
   notificacionesDinamicasPorSMS(correo: string, telefono:string, body:string):void {
-    const accountSid = 'ACbe3bd4f6acd6c025873be9084adcc04e'; // Your Account SID from www.twilio.com/console
-    const authToken = '08d26ed9f34d6f26e4e10f01f09c5710'; // Your Auth Token from www.twilio.com/console
+    const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+    const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
     
     const twilio = require('twilio');
     const client = new twilio(accountSid, authToken);
